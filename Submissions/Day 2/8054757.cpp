01d3c1018c0071760bc7158cf379ef8149394924
calibrik <matyusha_2004@mail.ru>
2023-01-19 23:20:26 +0400

    8054757
    fix negative nums

--- Submissions/Day 2/8054757.cpp
+++ Submissions/Day 2/8054757.cpp
@@ -14,7 +14,7 @@ vector<long long int> findPrimeNums(const vector<long long int>& arr)
 			newArr.push_back(arr[i]);
 			continue;
 		}
-		if (arr[i] % 2 == 0 || arr[i] == 1) continue;
+		if (arr[i] % 2 == 0 || arr[i] == 1|| arr[i]<=0) continue;
 		bool isPrime = true;
 		for (long long int j = 3; j < arr[i]; j += 2)
 		{
@@ -33,7 +33,7 @@ vector<long long int> findPrimeNums(const vector<long long int>& arr)
 int main()
 {
 	int timeStart = time(NULL);
-	vector<long long int> arr = { 1,2,3,4,5,6,7,62345,4047932051,78957486898967,9857502407 };
+	vector<long long int> arr = { 1,2,3,4,5,6,7,62345,-4047932051,78957486898967,9857502407 };
 	cout << "Input elements: ";
 	if (arr.size() == 0) return 0;
 	cout << arr[0];
